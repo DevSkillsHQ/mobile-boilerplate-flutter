2cc68fb84abd0be0f5872d00cdfeed4631ff9b65
Oscar Hägglund <oscarhagglund@live.com>
2022-01-15 15:48:47 +0100

    set up welcome screen with button that loads data

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_boilerplate_flutter/cubit/meal_cubit.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -7,7 +9,18 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Welcome'),
+        
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome'),
+            TextButton(
+                onPressed: () {
+                  BlocProvider.of<MealCubit>(context).getData();
+                },
+                child: Text('Press')),
+          ],
+        ),
       ),
     );
   }
