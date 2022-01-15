791cb416f7565ec40073221f2aee5f552dff266c
Oscar Hägglund <oscarhagglund@live.com>
2022-01-15 16:23:56 +0100

    meal selection screen now displays meals

--- lib/screens/meal_selection_screen.dart
+++ lib/screens/meal_selection_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_boilerplate_flutter/cubit/meal_cubit.dart';
+import 'package:mobile_boilerplate_flutter/data/meal.dart';
 
 class MealSelectionScreen extends StatelessWidget {
   const MealSelectionScreen({Key? key}) : super(key: key);
@@ -6,12 +9,48 @@ class MealSelectionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meal Selection'),
-      ),
-      body: const Center(
-        child: Text('Meal Selection Screen'),
+      body: BlocBuilder<MealCubit, MealState>(
+        builder: (context, state) {
+          if (state is MealLoaded) {
+            return _buildMealSelection(context, state.meals);
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
+}
+
+Widget _buildMealSelection(BuildContext context, List<Meal> meals) {
+  return GridView.builder(
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      childAspectRatio: 1.0,
+    ),
+    itemCount: 4,
+    itemBuilder: (context, index) {
+      return InkWell(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 2,
+          width: MediaQuery.of(context).size.width / 2,
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Image.network(meals[index].imageUrl)),
+              Expanded(
+                child: Text(meals[index].title, style: TextStyle(fontSize: 10))),
+            ],
+          ),
+        ),
+        onTap: () {
+          BlocProvider.of<MealCubit>(context).detailPage(meal: meals[index]);
+          Navigator.pop(context);
+        },
+      );
+    },
+  );
 }
