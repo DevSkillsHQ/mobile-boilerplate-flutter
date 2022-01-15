b308a8f216f2b1a3187286a64b06e9f847d61f9f
Oscar Hägglund <oscarhagglund@live.com>
2022-01-15 15:48:15 +0100

    setting up cubit behavior

--- lib/cubit/meal_cubit.dart
+++ lib/cubit/meal_cubit.dart
@@ -2,9 +2,33 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import 'package:mobile_boilerplate_flutter/data/meal.dart';
+import 'package:mobile_boilerplate_flutter/service/meal_service.dart';
 
 part 'meal_state.dart';
 
 class MealCubit extends Cubit<MealState> {
-  MealCubit() : super(MealInitial());
+  MealCubit({required this.data}) : super(MealInitial()) {
+    emit(MealInitial());
+  }
+
+  final MealService data;
+  late final meals;
+
+  void getData() async {
+    emit(MealLoading());
+    try {
+      meals = await data.getMeals();
+      emit(MealLoaded(meals));
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  detailPage({required Meal meal}) {
+    emit(MealDetail(meal));
+  }
+
+  goHome() {
+    emit(MealLoaded(meals));
+  }
 }
