11da4b5ce45aea3404ae516726aa72f340d4824e
Oscar Hägglund <oscarhagglund@live.com>
2022-01-16 10:05:57 +0100

    meal detail screen now working and styled

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,12 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile_boilerplate_flutter/cubit/meal_cubit.dart';
+import 'package:mobile_boilerplate_flutter/data/meal.dart';
 
-class MealDetailScreen extends StatelessWidget {
-  const MealDetailScreen({ Key? key }) : super(key: key);
+class MealDetailScreen extends StatefulWidget {
+  const MealDetailScreen({Key? key}) : super(key: key);
 
   @override
+  State<MealDetailScreen> createState() => _MealDetailScreenState();
+}
+
+class _MealDetailScreenState extends State<MealDetailScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(body: BlocBuilder<MealCubit, MealState>(
+      builder: (context, state) {
+        if (state is MealDetail) {
+          return _buildMealDetail(context, state.meal);
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    ));
   }
+}
+
+Widget _buildMealDetail(BuildContext context, Meal meal) {
+  return Container(
+    height: double.maxFinite,
+    width: double.maxFinite,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        TextButton(
+          child: Text('Go back'),
+          onPressed: () {
+            BlocProvider.of<MealCubit>(context).goHome();
+          },
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.maxFinite,
+                    height: MediaQuery.of(context).size.width * 0.95,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [Image.network(meal.imageUrl)],
+                    ),
+                  ),
+                  Text(meal.title,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  const SizedBox(height: 10),
+                  Text(meal.description!),
+                  const SizedBox(height: 10),
+                  Text('Ingredients',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                  Text(meal.ingredients!)
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }
