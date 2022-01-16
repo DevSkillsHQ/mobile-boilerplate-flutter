032b6d901e236c556198fd45d764e427388b4857
Oscar Hägglund <oscarhagglund@live.com>
2022-01-16 10:05:21 +0100

    corrected meal service

--- lib/service/meal_service.dart
+++ lib/service/meal_service.dart
@@ -7,9 +7,27 @@ class MealService {
   String baseUrl = "https://playground.devskills.co/api/rest/meal-roulette-app";
 
   Future<List<Meal>> getMeals() async {
-    var response = await http.get(Uri.parse(baseUrl + "/meals"));
+    var response =
+        await http.get(Uri.parse(baseUrl + "/meals/limit/4/offset/0"));
     if (response.statusCode == 200) {
       Map<String, dynamic> list = json.decode(response.body);
+      List<Meal> meals = [];
+
+      for (var meal in list["meal_roulette_app_meals_aggregate"]["nodes"]) {
+        meals.add(Meal.fromJson(meal));
+      }
+
+      return meals;
+    } else {
+      throw Exception('Failed to load meals');
+    }
+  }
+
+  Future<List<Meal>> getNextFourMeals(int id) async {
+    var response =
+        await http.get(Uri.parse(baseUrl + "/meals/limit/4/offset/$id"));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> list = jsonDecode(response.body);
       List<Meal> meals = [];
 
       for (var meal in list["meal_roulette_app_meals"]) {
@@ -22,11 +40,14 @@ class MealService {
     }
   }
 
-  Future<Meal> getMeal(int id) async {
-    var response = await http.get(Uri.parse(baseUrl + "/meals/$id"));
+  Future<Meal> getMeal(Meal meal) async {
+    var response = await http.get(Uri.parse(baseUrl + "/meals/${meal.id}"));
     if (response.statusCode == 200) {
-      var jsonResponse = json.decode(response.body);
-      return Meal.fromJson(jsonResponse);
+      Meal meal;
+      Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+      meal = Meal.fromJson(jsonResponse["meal_roulette_app_meals_by_pk"]);
+
+      return meal;
     } else {
       throw Exception('Failed to load meal');
     }
