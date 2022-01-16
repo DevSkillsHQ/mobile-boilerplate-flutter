1086ede7e3b30b0c188a4a75bddcef20a4174813
Oscar Hägglund <oscarhagglund@live.com>
2022-01-16 15:47:24 +0100

    made welcome screen prettier

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile_boilerplate_flutter/cubit/meal_cubit.dart';
+import 'package:mobile_boilerplate_flutter/widgets/app_text.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -8,18 +9,42 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Welcome'),
-            TextButton(
-                onPressed: () {
-                  BlocProvider.of<MealCubit>(context).getData();
-                },
-                child: Text('Press')),
-          ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.white,
+              Colors.black12,
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AppText(
+                text: 'Welcome to my code test!',
+                fontSize: 18,
+                isBold: true,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    
+                  ),
+                  onPressed: () {
+                    BlocProvider.of<MealCubit>(context).getData();
+                  },
+                  child: Text('Press to begin')),
+            ],
+          ),
         ),
       ),
     );
